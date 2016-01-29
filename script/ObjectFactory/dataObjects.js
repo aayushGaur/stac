8dd8620ac1508015c30226d124274baef1869f05
Aayush <agau12@student.monash.edu>
2016-01-29 14:41:23 +1100

    Update for issue 20
    If a line begins with "%" in the date file, it will be ignored by the
    parser.
    
    Cases which the current changes in the parser will not be able to handle
    when the information regarding a bus is commented :
    1. The bus is being used in some branch definition.
    2. The generator information regarding the bus is still present in the
    file.
    3. The bus location regarding the bus is still present.
    
    All the above cases create an anomaly in the file. Support for the above
    will be added if such cases are encountered.

--- script/ObjectFactory/dataObjects.js
+++ script/ObjectFactory/dataObjects.js
@@ -160,7 +160,10 @@
 			var crtContent = content[valIndexer];
 			//Updated the code to replace all the spaces by tab and then do the parsing.
 			crtContent = $.trim(crtContent);
-			if(crtContent !== "") {
+			
+			//Updated the condition for the content parsing to include the check for the '%' in the line.
+			//At present the location of the '%' is not relevant to the check as the symbol can be at any place in the beginning of the line.
+			if((crtContent !== "") && (crtContent.indexOf("%") === -1)) {
 				
 				if(crtContent.indexOf(' ') !== -1)	{
 					crtContent = crtContent.replace(/\s{1,}/g, '\t');
