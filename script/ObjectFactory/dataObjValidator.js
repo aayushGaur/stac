74cf3bc85e4e2052cd15cc3fa0af1fb7a4885f8c
Aayush <agau12@student.monash.edu>
2016-01-29 17:37:07 +1100

    Updated for issue 11.
    Updated the validation for error message - Thermal Limit is Negative.

--- script/ObjectFactory/dataObjValidator.js
+++ script/ObjectFactory/dataObjValidator.js
@@ -284,10 +284,14 @@
 			//errorList is not used for the  edge data
 			var ValidationError=[], errorList = [];
 			if(parseFloat(data.edgeData.rateA) < 0)  { 
-				error = true; 
-				errorList.push("Rate A");
-				validationErrorWarning.push({"key":"Error", "data":"Thermal Limit is Negative.","custom":"true","type":"error"});
-				LOGGER.addErrorMessage("Branch - "+ data.index + " (" +(data.edgeId) + ")" + " - Thermal Limit is Negative." ,data.edgeData.DOMID,"edge");
+				//Added an additional check for Issue 11 - separate If condition as this might be modified.....29/01/2016
+				if((((data.edgeData.angmax - data.edgeData.angmin) * Math.PI)/180) > 
+					(2 * Math.max(Math.abs(data.source.Vmax - data.target.Vmin), Math.abs(data.source.Vmin - data.target.Vmax)))) {					
+					error = true; 
+					errorList.push("Rate A");
+					validationErrorWarning.push({"key":"Error", "data":"Thermal Limit is Negative.","custom":"true","type":"error"});
+					LOGGER.addErrorMessage("Branch - "+ data.index + " (" +(data.edgeId) + ")" + " - Thermal Limit is Negative." ,data.edgeData.DOMID,"edge");
+				}
 			}
 			if(parseFloat(data.edgeData.angmin) > parseFloat(data.edgeData.angmax)) {
 				error = true;
